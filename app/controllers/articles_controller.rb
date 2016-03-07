fe1b35f8c6fcad48f4a6724d52ae59eb93b94adc
alekseylovchikov <svdmusic@gmail.com>
2016-03-07 21:26:53 +0300

    first commit

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,50 @@
+class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(articles_params)
+      flash[:notice] = 'Article Updated!'
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(articles_params)
+
+    if @article.save
+      flash[:notice] = 'Article was successfully created!'
+      redirect_to article_path(@article)
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    flash[:notice] = 'Article Deleted!'
+    redirect_to articles_path
+  end
+
+  private
+  def articles_params
+    params.require(:article).permit(:title, :description)
+  end
+end
